0cbe31733195d9f7274cc77cceb6cdb22fc11df6
Sanghyeong Kim <whangc789@naver.com>
2021-12-08 09:11:58 +0900

    초기 커밋.

---
+++ DeliveryClient1/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace DeliveryClient1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(368, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(368, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Phone";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(368, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(368, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Memo";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(429, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(429, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(262, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(429, 115);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(262, 21);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(426, 174);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(262, 21);
+            this.textBox4.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(426, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 25);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "주문";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "짜장면",
+            "짬뽕",
+            "볶음밥",
+            "탕수육"});
+            this.checkedListBox1.Location = new System.Drawing.Point(6, 0);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(356, 244);
+            this.checkedListBox1.TabIndex = 9;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(7, 250);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(918, 204);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Id";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Phone";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Address";
+            this.columnHeader3.Width = 200;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Menu";
+            this.columnHeader4.Width = 200;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Memo";
+            this.columnHeader5.Width = 200;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Time";
+            this.columnHeader6.Width = 150;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 470);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(930, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(121, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 492);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+    }
+}
+
---
+++ DeliveryClient1/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+
+
+namespace DeliveryClient1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Thread socketworker = new Thread(new ThreadStart(socketThread));
+            socketworker.Start();
+        }
+
+        private void socketThread()
+        {
+            // 1. 데이타패킷 조합
+            
+            DataPacket packet = new DataPacket();
+
+            packet.Id = textBox1.Text;
+            packet.Phone = textBox2.Text;
+            packet.Address = textBox3.Text;
+            packet.Memo = textBox4.Text;
+
+
+            string menu = null;
+            if (checkedListBox1.CheckedItems.Count != 0)
+            {
+
+                for (int x = 0; x < checkedListBox1.Items.Count; x++)
+                {
+                    if (checkedListBox1.GetItemChecked(x))
+                    {
+                        menu = menu + " " + checkedListBox1.Items[x].ToString();
+                    }
+                }
+            }
+            packet.Menu = menu;
+                        
+            // 연결
+            TcpClient client = new TcpClient(("127.0.0.1"), Convert.ToInt32("8888"));
+            NetworkStream stream = client.GetStream();
+            updateStatusInfo("Connected");
+
+            // 전송
+            byte[] buffer = GetBytes_Bind(packet);
+
+            stream.Write(buffer, 0, buffer.Length);
+            updateStatusInfo("주문 완료!");
+
+            
+            stream.Close();
+            client.Close();
+
+            // listview 에 추가하기
+            Invoke((MethodInvoker)delegate
+            {
+                ListViewItem i = new ListViewItem();
+
+                i.Text = textBox1.Text;
+                i.SubItems.Add(textBox2.Text);
+                i.SubItems.Add(textBox3.Text);
+                i.SubItems.Add(packet.Menu);
+                i.SubItems.Add(textBox4.Text);
+                i.SubItems.Add(DateTime.Now.ToString("MM-dd HH:mm:ss"));
+
+                listView1.Items.Add(i);
+            });
+        }
+
+        private void updateStatusInfo(string content)
+        {
+            Action del = delegate ()
+            {
+                toolStripStatusLabel1.Text = content;
+            };
+            Invoke(del);
+        }
+        // 패킷 사이즈
+        private const int BODY_BIND_SIZE = 20 + 20 + 20 + 100 + 100;
+
+        // 인증 패킷 구조체를 바이트 배열로 변환하는 함수
+        private byte[] GetBytes_Bind(DataPacket packet)
+        {
+            byte[] btBuffer = new byte[BODY_BIND_SIZE];
+
+            MemoryStream ms = new MemoryStream(btBuffer, true);
+            BinaryWriter bw = new BinaryWriter(ms);
+
+            
+            try
+            {
+                byte[] btName = new byte[20];
+                Encoding.UTF8.GetBytes(packet.Id, 0, packet.Id.Length, btName, 0);
+                bw.Write(btName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : {0}", ex.Message.ToString());
+            }
+
+            
+            try
+            {
+                byte[] btName = new byte[20];
+                Encoding.UTF8.GetBytes(packet.Phone, 0, packet.Phone.Length, btName, 0);
+                bw.Write(btName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : {0}", ex.Message.ToString());
+            }
+
+            try
+            {
+                byte[] btName = new byte[20];
+                Encoding.UTF8.GetBytes(packet.Address, 0, packet.Address.Length, btName, 0);
+                bw.Write(btName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : {0}", ex.Message.ToString());
+            }
+
+            try
+            {
+                byte[] btName = new byte[100];
+                Encoding.UTF8.GetBytes(packet.Menu, 0, packet.Menu.Length, btName, 0);
+                bw.Write(btName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : {0}", ex.Message.ToString());
+            }
+
+            try
+            {
+                byte[] btName = new byte[100];
+                Encoding.UTF8.GetBytes(packet.Memo, 0, packet.Memo.Length, btName, 0);
+                bw.Write(btName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : {0}", ex.Message.ToString());
+            }
+
+            bw.Close();
+            ms.Close();
+
+            return btBuffer;
+        }
+
+
+
+    }
+}
